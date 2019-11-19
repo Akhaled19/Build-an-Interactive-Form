ec3842e00135e4bd591b28f9921303ab5f85094f
Akhaled19 <akhaled@clarku.edu>
2019-11-18 23:01:57 -0500

    added js file & req

---
+++ interactive-form-v1/form.js
@@ -0,0 +1,47 @@
+
+                            //FOCUS ON THE FIRST FIELD//
+//using jQuery addEventListener (load?), have the cursor appear on the Name field
+
+
+                            //JOB ROLE SECTION//
+//using jQuery create a new element (text field) when user select 'other' from the Job Role menu 
+
+
+                             //T-SHIRT SECTION//
+//using jQuery(function?) to hide color options until t-shirt theme is selected 
+
+//using jQuery(function?) to show color options once t-shirt theme is selected 
+
+//using jQuery(what?) to prevent users from selecting two or activities that are at the same time 
+
+
+                             //ACTIVITY REGISTRATION//
+//using a jQuery(function?) to calculate & displays the cost of the selected activities right below the list 
+
+
+                            //DISPLAYING PAYMENT SECTIONS// 
+//using a jQuery(addEventListener?) (add e) to select credit card by default [like now w/out changes]
+
+//using a jQuery(function?) to match the payment option to payment option displayed on the page 
+
+//something about hidden other payment options once user select a payment option 
+
+
+                            //FORM VALIDATION//
+//using regexp to prevent refreshing the page when SUBMIT button is clicked UNTIL all the requirements have been filled 
+       //name field isn't empty 
+       //verified email address (name@email.com)
+       //at least one 'Register for Activities' checkbox is selected 
+       //credit card payment option: credit card num field contains 13 to 16 DIGITs, zip code contains  5-digit c, and cvv contains 3-digit
+
+
+                               //Form Validation Messages
+// same as above, but this is about filling out each filed (red until corrected)
+   //add a error message to each filed: the error message appears when the user begin to type & disappears later 
+   // add at least one error message that changes depending on the error. email field displays a different error message when the email field is empty than it does when the email address is formatted incorrectly.
+
+
+                            //Form Works Without JavaScript
+   //using the JQuery element that prevent web from breaking down when JS is disabled 
+     //all forms fields and payment information is displayed 
+     //including the 'other' field                          
